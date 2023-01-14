19ff8be4b100118a082b9323eb0217e317502210
ahoy-glitch <cesarjulio96746@gmail.com>
2023-01-14 19:35:13 -0300

    https://github.com/ahoy-glitch/It-is-zero.git

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,27 @@
-var number: Int = 35 + 34
-
-
+//Main fuction, that function will run our code
 fun main() {
     theClearGuy()
     println("Hi i'll tell you if a number is zero or not.")
     print("Press enter to continue.")
     val i = readLine()
-    if (i == "") theClearGuy()
+    if (i == "") {
+        //calling clear guy
+        theClearGuy()
+    } else {
+        //calling clear guy
+        theClearGuy()
+    }
 
     println("Digit the number: ")
+
+    //reading println
     val num = readln()
 
     theGuyWhoChecksTheNumber(num.toInt())
 }
 
 fun theGuyWhoChecksTheNumber (n: Int) {
+    //another call to clear guy
     theClearGuy()
     if (n == 0) {
         //smart
@@ -24,8 +31,9 @@ fun theGuyWhoChecksTheNumber (n: Int) {
         return println("It's not 0")
     }
 }
-
+/*
+simulating a console.clear(), bc System.out.exec("cls") don't work in IDE
+*/
 fun theClearGuy() {
-    for (x in 50 downTo 0 step 1) println();
+    for (x in 10000 downTo 0 step 1) println();
 }
-
